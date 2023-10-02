9e8d15782bf9523755530c71d855f56994660553
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2023-10-02 14:29:35 +0500

    Funntionality Integration

---
+++ data.js
@@ -0,0 +1,108 @@
+const quizData = [
+  {
+    id: "q1",
+    question: "What is the capital of France?",
+    options: [
+      { text: "Paris" },
+      { text: "London" },
+      { text: "Berlin" },
+      { text: "Madrid" },
+    ],
+    answerIndex: 0,
+  },
+  {
+    id: "q2",
+    question: "Which planet is known as the Red Planet?",
+    options: [
+      { text: "Mars" },
+      { text: "Venus" },
+      { text: "Jupiter" },
+      { text: "Saturn" },
+    ],
+    answerIndex: 0,
+  },
+  {
+    id: "q3",
+    question: "What is the largest mammal on Earth?",
+    options: [
+      { text: "Blue Whale" },
+      { text: "Elephant" },
+      { text: "Giraffe" },
+      { text: "Hippopotamus" },
+    ],
+    answerIndex: 0,
+  },
+  {
+    id: "q4",
+    question: "Which gas do plants absorb from the atmosphere?",
+    options: [
+      { text: "Oxygen" },
+      { text: "Carbon Dioxide" },
+      { text: "Nitrogen" },
+      { text: "Hydrogen" },
+    ],
+    answerIndex: 1,
+  },
+  {
+    id: "q5",
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: [
+      { text: "William Shakespeare" },
+      { text: "Charles Dickens" },
+      { text: "Jane Austen" },
+      { text: "Leo Tolstoy" },
+    ],
+    answerIndex: 0,
+  },
+  {
+    id: "q6",
+    question: "What is the largest organ in the human body?",
+    options: [
+      { text: "Liver" },
+      { text: "Heart" },
+      { text: "Skin" },
+      { text: "Lung" },
+    ],
+    answerIndex: 2,
+  },
+  {
+    id: "q7",
+    question: "Which country is known as the Land of the Rising Sun?",
+    options: [
+      { text: "China" },
+      { text: "India" },
+      { text: "Japan" },
+      { text: "South Korea" },
+    ],
+    answerIndex: 2,
+  },
+  {
+    id: "q8",
+    question: "What is the chemical symbol for gold?",
+    options: [{ text: "Au" }, { text: "Ag" }, { text: "Fe" }, { text: "Cu" }],
+    answerIndex: 0,
+  },
+  {
+    id: "q9",
+    question: "Who painted the Mona Lisa?",
+    options: [
+      { text: "Leonardo da Vinci" },
+      { text: "Pablo Picasso" },
+      { text: "Vincent van Gogh" },
+      { text: "Michelangelo" },
+    ],
+    answerIndex: 0,
+  },
+  {
+    id: "q10",
+    question: "What is the tallest mountain in the world?",
+    options: [
+      { text: "Mount Kilimanjaro" },
+      { text: "Mount Everest" },
+      { text: "Mount McKinley" },
+      { text: "Mount Fuji" },
+    ],
+    answerIndex: 1,
+  },
+];
+export default quizData;
--- main.js
+++ main.js
@@ -1,4 +1,69 @@
-const button = document.querySelector(".button");
-button.addEventListener("click", (e) => {
-  console.log("Clicked");
-});
+import QuizData from "./data.js";
+
+let counter = 0;
+let listContainer;
+
+function loadData() {
+  displayRemainingQuest();
+  displayQuestion();
+}
+function displayRemainingQuest() {
+  let remainingQuestion = document.querySelector(".question-length");
+  remainingQuestion.innerHTML = `QUESTION ${counter} / ${QuizData.length}`;
+}
+function displayQuestion() {
+  let question = document.querySelector(".question");
+  question.innerText = QuizData[counter].question;
+  createElementsAndTypes();
+}
+function createElementsAndTypes() {
+  let inputRadio = document.createElement("input");
+  let liElement = document.createElement("li");
+  let label = document.createElement("label");
+  inputRadio.type = "radio";
+  inputRadio.id = "opt_1";
+  let choices = QuizData[counter].options;
+  listContainer = document.querySelector(".choice-container");
+  appendElements(liElement, inputRadio, label);
+  displayChoices(choices, label);
+}
+
+function appendElements(liElement, inputRadio, label) {
+  liElement.appendChild(inputRadio);
+  liElement.appendChild(label);
+  listContainer.appendChild(liElement);
+}
+function displayChoices(choices, label) {
+  for (const choice of choices) {
+    let key = Object.keys(choice);
+    label.innerText = choice[key];
+    // createElementsAndTypes();
+  }
+}
+
+const buttonNext = document.querySelector(".btnNext");
+const buttonPrev = document.querySelector(".btnPrevious");
+buttonNext.addEventListener("click", btnFunctionality);
+buttonPrev.addEventListener("click", btnFunctionality);
+
+function btnFunctionality() {
+  if (listContainer.hasChildNodes()) {
+    let element = listContainer.children;
+    listContainer.remove(element);
+    let formData = document.querySelector(".choice-list");
+    let ulElement = document.createElement("ul");
+    ulElement.classList.add("choice-container");
+    formData.appendChild(ulElement);
+    loadData();
+  }
+}
+function nextQuestion() {
+  counter++;
+  btnFunctionality();
+}
+function prevQuestion() {
+  counter--;
+  btnFunctionality();
+}
+
+loadData();
